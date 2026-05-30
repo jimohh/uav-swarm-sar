6e83fd8b4605209341fe98f29e4fb7876770c2b4
Jimoh Muhammad <jimohmuhammad20@gmail.com>
2026-05-30 21:02:46 +0100

    Fix setup.py to include launch files

--- ros2_ws/src/sar_planning/setup.py
+++ ros2_ws/src/sar_planning/setup.py
@@ -1,30 +1,33 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'sar_planning'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='muhammad',
-    maintainer_email='jimohmuhammad20@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer='maintainer',
+    maintainer_email='maintainer@example.com',
+    description='SAR planning stack',
+    license='MIT',
+    tests_require=['pytest'],
     entry_points={
-    'console_scripts': [
-        'probability_map_node = sar_planning.probability_map_node:main',
+        'console_scripts': [
+            'probability_map_node = sar_planning.probability_map_node:main',
+            'waypoint_selector    = sar_planning.waypoint_selector:main',
+            'apf_navigator        = sar_planning.apf_navigator:main',
+            'ekf_node             = sar_planning.ekf_node:main',
         ],
     },
 )
