d8bb052103142ed3a9130691c700945b728f2fdd
Jimoh Muhammad <jimohmuhammad20@gmail.com>
2026-05-31 20:56:32 +0100

    Fix setup.py: add thermal_camera, rf_doppler, yolo11s entry points

--- ros2_ws/src/sar_planning/setup.py
+++ ros2_ws/src/sar_planning/setup.py
@@ -23,11 +23,14 @@ setup(
     license='MIT',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-            'probability_map_node = sar_planning.probability_map_node:main',
-            'waypoint_selector    = sar_planning.waypoint_selector:main',
-            'apf_navigator        = sar_planning.apf_navigator:main',
-            'ekf_node             = sar_planning.ekf_node:main',
-        ],
-    },
+    'console_scripts': [
+        'probability_map_node = sar_planning.probability_map_node:main',
+        'waypoint_selector    = sar_planning.waypoint_selector:main',
+        'apf_navigator        = sar_planning.apf_navigator:main',
+        'ekf_node             = sar_planning.ekf_node:main',
+        'thermal_camera_node  = sar_planning.thermal_camera_node:main',
+        'rf_doppler_stub      = sar_planning.rf_doppler_stub:main',
+        'yolo11s_detector     = sar_planning.yolo11s_detector:main',
+    ],
+},
 )
